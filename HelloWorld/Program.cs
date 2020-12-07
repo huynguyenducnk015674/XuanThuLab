277a2057b237b2f8d535ac684a9a0cd5f71d8e8b
Huy.NguyenDuc <Huy.NguyenDuc@CTY-LTP-702>
2020-12-07 17:22:29 +0700

    New Project 54_ Overview Netcore with Helloword

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Build -  tạo các dịch vụ đã đăng ký trả về WebHost
+            // Run - chạy ứng dụng web
+            //var hostBuilder=CreateHostBuilder(args);
+            //var iHost = hostBuilder.Build();
+            //iHost.Run();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // webBuilder đối tượng lớp WebHostBuilder để cấu hình, đăng ký các dịch vụ ứng dụng Web
+                    // UseStartup chỉ ra lớp khởi chạy ứng dụng (đăng ký dịch vụ)
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
