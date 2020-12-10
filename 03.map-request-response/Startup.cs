386be91bb9c4333f53f884ac3e093dc37d55c760
huynguyenducnk015674 <huynd.aptech@hotmail.com>
2020-12-11 00:08:16 +0700

    map-request-response finish

--- 03.map-request-response/Startup.cs
+++ 03.map-request-response/Startup.cs
@@ -16,6 +16,8 @@ namespace _03.map_request_response
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +27,8 @@ namespace _03.map_request_response
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
@@ -32,9 +36,46 @@ namespace _03.map_request_response
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    string menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+                    string content = HtmlHelper.HtmlTrangchu();
+                    string html = HtmlHelper.HtmlDocument("Home Page", menu+ content);
+                    await context.Response.WriteAsync(html);
+                });
+                endpoints.Map("/RequestInfo/abc/xyz", async context =>
+                {
+                    string menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+                    string requestInfo = RequestProcess.RequestInfo(context.Request).HtmlTag("div", "container");
+                    string html = HtmlHelper.HtmlDocument("Thông tin Request", menu + requestInfo);
+                    await context.Response.WriteAsync(html);
+                });
+                endpoints.MapGet("/Encoding", async context =>
+                {
+                    await context.Response.WriteAsync("Endcoding");
+                });
+                endpoints.MapGet("/Cookies", async context =>
+                {
+                    await context.Response.WriteAsync("Cookies");
                 });
             });
+            app.Map("/Json", (app) =>
+            {
+                app.Run(  async context =>
+                {
+                    await context.Response.WriteAsync(RequestProcess.PrintJson(context.Request).Result);
+                });                
+            });
+            app.Map("/Form", (app) =>
+             {
+                 app.Run(async context =>
+                {
+                    string menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+                    string formhtml = await RequestProcess.FormProcess(context.Request);
+                    formhtml = formhtml.HtmlTag("div", "container");
+                    string html = HtmlHelper.HtmlDocument("Form Post", (menu + formhtml));
+                    await context.Response.WriteAsync(html);
+
+                });
+             });
         }
     }
 }
